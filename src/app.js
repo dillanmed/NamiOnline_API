dd8c065f7a9631aee07ae482ee71e3bc81c189bb
Samuel Rocha <saamueldev@gmail.com>
2026-05-17 10:43:58 -0300

    crud de consulta, foi adicionado especialidade, consulta, guia e agenda medico.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const userRoutes = require("./routes/usuarioRoutes");
 const medicoRoutes = require("./routes/medicoRoutes");
 const pacienteRoutes = require("./routes/pacienteRoutes");
+const guiaRoutes = require("./routes/guiaRoutes");
+const especialidadeRoutes = require("./routes/especialidadeRoutes");
+const consultaRoutes = require("./routes/consultaRoutes");
+const agendaMedicoRoutes = require("./routes/agendaMedicoRoutes");
+
 
 const app = express();
 
@@ -12,5 +17,9 @@ app.use(express.json());
 app.use("/usuarios", userRoutes);
 app.use("/medicos", medicoRoutes);
 app.use("/pacientes", pacienteRoutes);
+app.use("/guias", guiaRoutes);
+app.use("/especialidades", especialidadeRoutes);
+app.use("/consultas", consultaRoutes);
+app.use("/agenda-medicos", agendaMedicoRoutes);
 
 module.exports = app;
