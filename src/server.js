ec4b9e5a260b2612f678ecf487dc18de7892f12a
Samuel Rocha <saamueldev@gmail.com>
2026-05-18 20:22:55 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,45 +1,9 @@
 require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-
+const app = require("./app");
 const connectDatabase = require("./config/database");
 
-const userRoutes = require("./routes/usuarioRoutes");
-const medicoRoutes = require("./routes/medicoRoutes");
-const pacienteRoutes = require("./routes/pacienteRoutes");
-const categoriaExameRoutes = require("./routes/categoriaExameRoutes");
-const tipoExameRoutes = require("./routes/tipoExameRoutes");
-const agendamentoExameRoutes = require("./routes/agendamentoExameRoutes");
-const retornoRoutes = require("./routes/retornoRoutes");
-const notificacaoRoutes = require("./routes/notificacaoRoutes");
-const configuracaoRoutes = require("./routes/configuracaoRoutes");
-const avatarRoutes = require("./routes/Avatarroutes");
-const guiaRoutes = require("./routes/guiaRoutes");
-const especialidadeRoutes = require("./routes/especialidadeRoutes");
-const consultaRoutes = require("./routes/consultaRoutes");
-const agendaMedicoRoutes = require("./routes/agendaMedicoRoutes");
-
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/usuarios", userRoutes);
-app.use("/medicos", medicoRoutes);
-app.use("/pacientes", pacienteRoutes);
-app.use("/categorias-exames", categoriaExameRoutes);
-app.use("/tipos-exames", tipoExameRoutes);
-app.use("/agendamentos-exames", agendamentoExameRoutes);
-app.use("/retornos", retornoRoutes);
-app.use("/notificacoes", notificacaoRoutes);
-app.use("/configuracoes", configuracaoRoutes);
-app.use("/configuracoes", avatarRoutes);
-app.use("/guias", guiaRoutes);
-app.use("/especialidades", especialidadeRoutes);
-app.use("/consultas", consultaRoutes);
-app.use("/agendas-medicos", agendaMedicoRoutes);
 
 async function startServer() {
   try {
